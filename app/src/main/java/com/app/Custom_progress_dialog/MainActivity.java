b176f82cd0305348bdfdfebb51ceb89b079d6d74
yash123482 <yash.a@amuratech.com>
2017-03-21 13:18:17 +0530

    Test

--- app/src/main/java/com/app/Custom_progress_dialog/MainActivity.java
+++ app/src/main/java/com/app/Custom_progress_dialog/MainActivity.java
@@ -13,5 +13,24 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*Dialog progressDialog = new Dialog(this);
+        progressDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        progressDialog.setContentView(R.layout.custom_dialog_progress);
+        progressDialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+        progressDialog.setCancelable(false);
+
+        progressDialog.show();*/
+
+        /*
+         * To Dismiss or Cancel the ProgressDialog use.
+         *
+         *  if(progressDialog != null)
+                        {
+                            progressDialog.cancel();
+                            progressDialog.hide();
+                        }
+          */
+
     }
 }
